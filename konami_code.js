85e7d0dcd2466d3786c80dc4649334b8c2eaf9fa
Ting Chung <tchung682@gmail.com>
2020-05-14 21:53:02 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,5 +16,5 @@ function init() {
   document.body.addEventListener("keydown", (event) => {
   // Now, how can we check for which specific key was pressed?
   alert("Hurray!" + event.key + "was pressed")
-  }
+})
 }
