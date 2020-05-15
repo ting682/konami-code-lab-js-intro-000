1ed06183427b06fd4fbef1e343f2d42c998a92bc
Ting Chung <tchung682@gmail.com>
2020-05-15 00:18:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,25 @@ const codes = [
 
 function init() {
   // your code here
+  var index;
+  document.body.addEventListener("keydown", (event) => {
+  // Now, how can we check for which specific key was pressed?
+  console.log( event.key + " was pressed")
+  const key = event.key
+  var codeCompare = codes
+  //debugger
+  if(event.key === codeCompare[0]){
+    codeCompare.shift()
+    console.log(codeCompare.length)
+
+    if(codeCompare.length === 0){
+      alert("Hurray")
+      return true;
+    }
+  } else{
+    console.log("not correct")
+    return false;
+  }
+
+})
 }
